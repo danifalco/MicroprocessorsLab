39a2dea98c535fd5e165fe77bff563b9c9cd20b7
Daniel <daniwero2003@gmail.com>
2023-11-03 10:30:38 +0000

    Added PortD input

--- main.s
+++ main.s
@@ -8,15 +8,18 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movlw	0xFF
+	movwf	TRISD, A	    ; Port D all inputs
+        movlw 	0x0
+	movwf	TRISC, A	    ; Port C all outputs
+		    
 	bra 	test
 loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
+	movff 	0x06, PORTC	    ; Move whatever is in 06 to Port C
+	incf 	0x06, W, A	    ; increment 0x06 by 1, copy the value in W
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movwf	0x06, A		    ; Copy W to 0x06
+	movf 	PORTD, W, A	    ; Copy Port D to W
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
